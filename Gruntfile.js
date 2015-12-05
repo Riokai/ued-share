eed3143056de2f207c5fc44b3ecc2cafbeff47d6
Kai <zengkai2009@foxmail.com>
2015-12-05 10:52:03 +0800

    add demo02 example

--- Gruntfile.js
+++ Gruntfile.js
@@ -26,8 +26,51 @@ module.exports = function(grunt) {
           livereload: '<%= connect.options.livereload %>'
         },
         files: [
-          'demo01/{,**/}*.html'
+          'demo01/{,**/}*.html',
+
+          'demo02/{,**/}*.html',
+          'demo02/css/{,**/}*.css'
         ]
+      },
+
+      sass: {
+        files: ['demo02/scss/*.scss'],
+        // tasks: ['sass:demo02']
+        tasks: ['sass:demo02', 'autoprefixer:demo02']
+      }
+    },
+
+    sass: {
+      options: {
+        sourcemap: 'none',
+        // expanded, nested, compressed, compact
+        style: 'expanded',
+        compass: false
+      },
+
+      demo02: {
+        files: [{
+          expand: true,
+          cwd: 'demo02/scss',
+          src: ['*.scss'],
+          dest: 'demo02/css',
+          ext: '.css'
+        }]
+      }
+    },
+
+    autoprefixer: {
+      options: {
+        browsers: ['> 0%']
+      },
+
+      demo02: {
+        files: [{
+          expand: true,
+          cwd: 'demo02/css/',
+          src: '{,*/}*.css',
+          dest: 'demo02/css/'
+        }]
       }
     }
 
@@ -40,7 +83,9 @@ module.exports = function(grunt) {
 
   grunt.registerTask('demo02', [
     'connect',
-    'watch:livereload'
+    'sass:demo02',
+    'autoprefixer:demo02',
+    'watch'
   ]);
 
 };
